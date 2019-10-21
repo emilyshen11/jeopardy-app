f6c3d4017325cb14e9a628bd3e2236a1db66e101
Emily Shen <emilyamyshen@gmail.com>
2019-10-20 22:40:37 -0400

    Fetch categories upon server startup

--- server.js
+++ server.js
@@ -3,6 +3,20 @@ const bodyParser = require('body-parser');
 const request = require('request');
 const app = express()
 
+const JEOPARDY_API_URL = 'http://jservice.io'
+const CLUES_ENDPOINT = '/api/clues'
+const CATEGORIES_ENDPOINT = '/api/categories'
+
+let categories;
+
+request(`${JEOPARDY_API_URL}${CATEGORIES_ENDPOINT}?count=100`, function(err, response, body) {
+  if (err) {
+    console.log('Unable to fetch categories')
+  } else {
+    categories = body
+  }
+});
+
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs')
@@ -14,9 +28,10 @@ app.get('/', function(req, res) {
 app.post('/search', function(req, res) {
   let min_date = req.body.min_date;
   let max_date = req.body.max_date;
-  let url = `http://jservice.io/api/clues?min_date=${min_date}&max_date=${max_date}`
+  let url = `${CLUES_ENDPOINT}?min_date=${min_date}&max_date=${max_date}`
 
   request(url, function(err, response, body) {
+    console.log(body)
     if (err) {
       res.render('index', { clues: null, error: 'Error, please try again' });
     } else {
