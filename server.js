4d20eb9c484108e5423d363384242adccfa681fc
Emily Shen <emilyamyshen@gmail.com>
2019-10-22 19:02:18 -0400

    Save filters from search to search, clear filters button

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const CLUES_ENDPOINT = '/api/clues';
 const CATEGORIES_ENDPOINT = '/api/categories';
 
 let categories = [];
+let filters = [];
 
 request(`${JEOPARDY_API_URL}${CATEGORIES_ENDPOINT}?count=100`, function(err, response, body) {
   if (err) {
@@ -23,29 +24,32 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
-  res.render('index', { clues: null, categories: categories, error: null});
+  res.render('index', { clues: null, categories: categories, filters: filters, error: null});
 });
 
 app.post('/search', function(req, res) {
-  let min_date = req.body.min_date;
-  let max_date = req.body.max_date;
-  let category = req.body.category;
-  let value = req.body.value;
+  let filters = req.body;
+  console.log(filters);
+
+  let min_date = filters.min_date;
+  let max_date = filters.max_date;
+  let category = filters.category;
+  let value = filters.value;
   let url = `${JEOPARDY_API_URL}${CLUES_ENDPOINT}?min_date=${min_date}&max_date=${max_date}&category=${category}&value=${value}`
 
   request(url, function(err, response, body) {
     log.info(`Request received: ${url}`);
     if (err) {
       log.error(err);
-      res.render('index', { clues: null, categories: categories, error: 'Error, please try again' });
+      res.render('index', { clues: null, categories: categories, filters: filters, error: 'Error, please try again' });
     } else {
       let clues = JSON.parse(body);
       if (clues.length === 0) {
         log.info('Could not find any clues');
-        res.render('index', { clues: null, categories: categories, error: `No clues found`});
+        res.render('index', { clues: null, categories: categories, filters: filters, error: `No clues found`});
       } else {
         log.info(`Returned ${clues.length} clues`);
-        res.render('index', { clues: clues, categories: categories, error: null });
+        res.render('index', { clues: clues, categories: categories, filters: filters, error: null });
       }
     }
   });
@@ -53,7 +57,7 @@ app.post('/search', function(req, res) {
 
 app.use(function (err, req, res, next) {
   log.error(err);
-  res.render('index', { clues: null, categories: categories, error: 'Error, please try again' });
+  res.render('index', { clues: null, categories: categories, filters: filters, error: 'Error, please try again' });
 });
 
 app.listen(3000, function () {
