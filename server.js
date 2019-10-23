94734e794590bf4fb52cf2b0099b98717fff2f61
Emily Shen <emilyamyshen@gmail.com>
2019-10-22 21:01:41 -0400

    Add Procfile and port setting

--- server.js
+++ server.js
@@ -60,6 +60,6 @@ app.use(function (err, req, res, next) {
   res.render('index', { clues: null, categories: categories, filters: filters, error: 'Error, please try again' });
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Jeopardy app listening on port 3000!');
 });
